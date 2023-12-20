64878ea86b0bcd4b614c18fe4013edc7cca65315
ArtemPapusha <art.papusha@gmail.com>
2023-12-20 18:23:31 +0900

    [feature] fetch_v.0.01

---
+++ src/components/Fetch.js
@@ -0,0 +1,33 @@
+import ListCards from '@/components/ListCards';
+import ProductAPI from '@/services/PoductAPI';
+const listCards = new ListCards();
+const productAPI = new ProductAPI();
+
+class Fetch {
+
+  constructor() {
+    listCards.render();
+  }
+
+  listProductsLoader = (show) => {
+
+    show ? listCards.addSkeletonCards(12) : listCards.removeSkeletonCards();
+    
+  }
+  
+  addCards = (data) => {
+      data?.forEach(card => {
+        listCards.addCard(card);
+      });
+ 
+  }
+
+  fetchCards = async () => {
+  
+    await productAPI.getProducts(this.listProductsLoader, this.addCards);
+
+  }
+
+}
+
+export default Fetch;
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,36 +1,9 @@
 import '~/modules/index.scss';
 
-import ListCards from '@/components/ListCards';
-import ProductAPI from '@/services/PoductAPI';
-import Loading from '@/components/Loading';
+import Fetch from '@/components//Fetch';
+
+const fetchProduct = new Fetch();
 
 
-const loading = new Loading()
-const listCards = new ListCards();
-const productAPI = new ProductAPI();
-
-
-function listProductsLoader(show) {
-
-  show ? listCards.addSkeletonCards(12) : listCards.removeSkeletonCards();
-  
-}
-
-function addCards(data) {
-    data?.forEach(card => {
-      listCards.addCard(card);
-    });
-}
-
-
-
-async function fetchCards() {
-
-  await productAPI.getProducts(listProductsLoader, addCards);
-
-}
-
-listCards.render();
-
-fetchCards();
+fetchProduct.fetchCards();
 
